035999ef5d941195035fc38a7411c648adb0533d
Rizky <mail.rizkytegar@gmail.com>
2025-04-07 22:50:14 +0700

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,11 +1,10 @@
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
-import binascii
+from pathlib import Path
 
-cipher_hex = "632EA096F27B6662A2386DE9F73C5698B51D4E0805172B749602FED580A8EFA5F484"
 key = b"gnzLCH7vxt4R8HyxFWipUPIFpcSTw9Ir"
 nonce = b"gnzLCH7vxt4R"
 
-ciphertext = binascii.unhexlify(cipher_hex)
+ciphertext = Path("example.zxd").read_bytes()
 
 chacha = ChaCha20Poly1305(key)
 plaintext = chacha.decrypt(nonce, ciphertext, None)
