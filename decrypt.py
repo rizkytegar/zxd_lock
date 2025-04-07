7f4da76406b325b6c5cef959d35b0e0928237e77
Rizky <mail.rizkytegar@gmail.com>
2025-04-07 22:51:46 +0700

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,10 +1,16 @@
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 from pathlib import Path
+import binascii
 
 key = b"gnzLCH7vxt4R8HyxFWipUPIFpcSTw9Ir"
 nonce = b"gnzLCH7vxt4R"
 
-ciphertext = Path("example.zxd").read_bytes()
+data = Path("example.zxd").read_bytes()
+
+try:
+    ciphertext = binascii.unhexlify(data.strip())
+except Exception:
+    ciphertext = data  # asumsi data sudah biner
 
 chacha = ChaCha20Poly1305(key)
 plaintext = chacha.decrypt(nonce, ciphertext, None)
